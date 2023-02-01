a715d9787fe1108393d99225856269d2002919f3
= <=>
2023-02-01 23:11:41 +0700

    make migration

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log('bismillah');
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
