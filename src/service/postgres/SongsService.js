43fa72f12327f704811b08a52a0937c129567756
= <=>
2023-02-06 12:21:44 +0700

    add query search by title or performer service

--- src/service/postgres/SongsService.js
+++ src/service/postgres/SongsService.js
@@ -30,7 +30,20 @@ class SongsService {
     return songId;
   }
 
-  async getSongs() {
+  async getSongs(title, performer) {
+    if (title && performer) {
+      const result = await this._pool.query(`SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE '%${title.toLowerCase()}%' AND LOWER(performer) LIKE '%${performer.toLowerCase()}%'`);
+      return result.rows;
+    }
+    if (title) {
+      const result = await this._pool.query(`SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE '%${title.toLowerCase()}%'`);
+      return result.rows;
+    }
+    if (performer) {
+      const result = await this._pool.query(`SELECT id, title, performer FROM songs WHERE LOWER(performer) LIKE '%${performer.toLowerCase()}%'`);
+      return result.rows;
+    }
+
     const result = await this._pool.query('SELECT songs.id, songs.title, songs.performer FROM songs');
     return result.rows;
   }
